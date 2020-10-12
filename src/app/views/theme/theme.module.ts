1dec2a84edd7aff2eab67ebeede6255d18e21157
Danilo Cadena - Bussine <danilo.cadena@ramedicas.com>
2020-10-12 15:20:40 -0500

    agregando SweetAlert2Module a thememodule

--- src/app/views/theme/theme.module.ts
+++ src/app/views/theme/theme.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavigationComponent } from './navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,8 +8,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+
+import { NavigationComponent } from './navigation/navigation.component';
+import { SweetalertComponent } from './sweetalert/sweetalert.component';
+
 const COMPONENTS = [
   NavigationComponent,
+  SweetalertComponent,
 ];
 
 @NgModule({
@@ -20,6 +25,7 @@ const COMPONENTS = [
     CommonModule,
     RouterModule,
     LayoutModule,
+    SweetAlert2Module.forChild(),
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
