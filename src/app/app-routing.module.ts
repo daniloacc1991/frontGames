24cac517063e5497d128d641121d7b49a485ee64
Danilo Cadena - Bussine <danilo.cadena@ramedicas.com>
2020-10-12 17:13:05 -0500

    creación ruta juegos en el app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'clientes',
+        loadChildren: () => import('./views/pages/cliente/cliente.module').then(m => m.ClienteModule),
+      },
+      {
+        path: 'juegos',
+        loadChildren: () => import('./views/pages/juegos/juegos.module').then(m => m.JuegosModule),
+      }
+    ],
+  },
+  { path: '', redirectTo: 'clientes', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { RootStoreModule } from './root-store/root-store.module';
+import { ThemeModule } from "./views/theme/theme.module";
+import { HttpClientModule } from '@angular/common/http';
+
+export function provideSwal() {
+  return import('sweetalert2').then(({ default: Swal }) => Swal.mixin({
+    cancelButtonText: 'Cancelar',
+    confirmButtonText: 'Aceptar',
+  }));
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +25,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ThemeModule,
+    RootStoreModule,
+    SweetAlert2Module.forRoot({ provideSwal }),
   ],
   providers: [],
   bootstrap: [AppComponent]
